e65e2b8cf7f75b10e99a957915480863188f46e2
Bruno Castro <bruno.hen.castro@gmail.com>
2023-10-10 20:48:40 -0300

    Ajustes menores

--- scripts/main-bkp.js
+++ scripts/main-bkp.js
@@ -1,40 +1,42 @@
-window.alert ('Preencha seus dados e responda as questões!');
-
+let confirmar = document.getElementById('enviar');
 function enviar() {
     window.alert('Respostas enviadas, boa sorte!');
 }
-document.querySelector('#enviar').onclick=enviar;
+confirmar.onclick=enviar;
 
+let zerar = document.getElementById('limpar');
 function limpar() {
-    window.alert('Valores apagados!')
-    document.getElementById('total')
-    total.innerHTML = 0
+    document.getElementById('total');
+    total.innerHTML = 0;
 }
-document.getElementById('limpar').onclick=limpar;
+zerar.onclick=limpar;
 
+let totalCelulas = document.getElementById('calcular');
 function somar() {
-    var cel1 = Number(document.getElementById('cel_1').value);
-    var cel2 = Number(document.getElementById('cel_2').value);
-    var cel3 = Number(document.getElementById('cel_3').value);
-    var cel4 = Number(document.getElementById('cel_4').value);
-    var cel5 = Number(document.getElementById('cel_5').value);
-    var cel6 = Number(document.getElementById('cel_6').value);
-    var cel7 = Number(document.getElementById('cel_7').value);
-    var cel8 = Number(document.getElementById('cel_8').value);
-    var cel9 = Number(document.getElementById('cel_9').value);
-    var cel10 = Number(document.getElementById('cel_10').value);
-    var cel11 = Number(document.getElementById('cel_11').value);
-    var cel12 = Number(document.getElementById('cel_12').value);
-    var cel13 = Number(document.getElementById('cel_13').value);
-    var cel14 = Number(document.getElementById('cel_14').value);
+    let cel1 = Number(document.getElementById('cel_1').value);
+    let cel2 = Number(document.getElementById('cel_2').value);
+    let cel3 = Number(document.getElementById('cel_3').value);
+    let cel4 = Number(document.getElementById('cel_4').value);
+    let cel5 = Number(document.getElementById('cel_5').value);
+    let cel6 = Number(document.getElementById('cel_6').value);
+    let cel7 = Number(document.getElementById('cel_7').value);
+    let cel8 = Number(document.getElementById('cel_8').value);
+    let cel9 = Number(document.getElementById('cel_9').value);
+    let cel10 = Number(document.getElementById('cel_10').value);
+    let cel11 = Number(document.getElementById('cel_11').value);
+    let cel12 = Number(document.getElementById('cel_12').value);
+    let cel13 = Number(document.getElementById('cel_13').value);
+    let cel14 = Number(document.getElementById('cel_14').value);
 
-    var soma = cel1 + cel2 + cel3 + cel4 + cel5 + cel6 + cel7 + cel8 + cel9 + cel10 + cel11 + cel12 + cel13 + cel14;
-    document.getElementById('total')
-
+    let soma = cel1 + cel2 + cel3 + cel4 + cel5 + cel6 + cel7 + cel8 + cel9 + cel10 + cel11 + cel12 + cel13 + cel14;
+    document.getElementById('total');
     total.innerHTML = soma;
 }
-document.getElementById('calcular').onclick=somar
+totalCelulas.onclick = somar
 
+let RBC = prompt('Digite o valor de RBC');
+document.getElementById('valor-rbc');
+span.innerHTML = RBC;
 
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,22 @@ function limpar() {
 document.getElementById('limpar').onclick=limpar;
 
 function somar() {
-    var cel1 = Number(document.getElementById('cel_1').value);
-    var cel2 = Number(document.getElementById('cel_2').value);
-    var cel3 = Number(document.getElementById('cel_3').value);
-    var cel4 = Number(document.getElementById('cel_4').value);
-    var cel5 = Number(document.getElementById('cel_5').value);
-    var cel6 = Number(document.getElementById('cel_6').value);
-    var cel7 = Number(document.getElementById('cel_7').value);
-    var cel8 = Number(document.getElementById('cel_8').value);
-    var cel9 = Number(document.getElementById('cel_9').value);
-    var cel10 = Number(document.getElementById('cel_10').value);
-    var cel11 = Number(document.getElementById('cel_11').value);
-    var cel12 = Number(document.getElementById('cel_12').value);
-    var cel13 = Number(document.getElementById('cel_13').value);
-    var cel14 = Number(document.getElementById('cel_14').value);
+    let cel1 = Number(document.getElementById('cel_1').value);
+    let cel2 = Number(document.getElementById('cel_2').value);
+    let cel3 = Number(document.getElementById('cel_3').value);
+    let cel4 = Number(document.getElementById('cel_4').value);
+    let cel5 = Number(document.getElementById('cel_5').value);
+    let cel6 = Number(document.getElementById('cel_6').value);
+    let cel7 = Number(document.getElementById('cel_7').value);
+    let cel8 = Number(document.getElementById('cel_8').value);
+    let cel9 = Number(document.getElementById('cel_9').value);
+    let cel10 = Number(document.getElementById('cel_10').value);
+    let cel11 = Number(document.getElementById('cel_11').value);
+    let cel12 = Number(document.getElementById('cel_12').value);
+    let cel13 = Number(document.getElementById('cel_13').value);
+    let cel14 = Number(document.getElementById('cel_14').value);
 
-    var soma = cel1 + cel2 + cel3 + cel4 + cel5 + cel6 + cel7 + cel8 + cel9 + cel10 + cel11 + cel12 + cel13 + cel14;
+    let soma = cel1 + cel2 + cel3 + cel4 + cel5 + cel6 + cel7 + cel8 + cel9 + cel10 + cel11 + cel12 + cel13 + cel14;
     document.getElementById('total')
 
     total.innerHTML = soma;
